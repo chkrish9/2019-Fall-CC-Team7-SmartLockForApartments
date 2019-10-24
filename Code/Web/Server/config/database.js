0db455763e937fb9ead1a9950027c3129b831d89
Murali <murali.24.4.91@gmail.com>
2019-10-24 15:43:42 -0500

    web code set up

---
+++ Code/Web/Server/config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+    database : 'mongodb+srv://admin:admin@cluster0-yhy6g.mongodb.net/test?retryWrites=true&w=majority',
+    secret : 'SMARTDOORLOOK',
+}
---
+++ Code/Web/Server/routes/routes.js
@@ -0,0 +1,4 @@
+var users = require('../modules/users/userrouter');
+module.exports = function router(app){
+    app.use('/users',users);
+};
