8c881228afca70f5bd5465ced00ea0c02413962d
Murali <murali.24.4.91@gmail.com>
2019-10-31 10:15:10 -0500

    Change in email sending

--- Code/Web/Server/modules/users/userrouter.js
+++ Code/Web/Server/modules/users/userrouter.js
@@ -7,150 +7,132 @@ const config = require("../../config/passport");
 const email = require("../../config/email");
 
 //Get
-router.get(
-  "/all",
-  config.checkToken,
-  (req, res, next) => {
-    User.getUsers((err, data) => {
-      data.forEach(element => {
-        element.dateOfJoin = new Date(element.dateOfJoin).toLocaleDateString(
-          "en"
-        );
-      });
-      res.json(data);
-    });
-  }
-);
-
-router.get(
-  "/get/:name",
-  config.checkToken,
-  (req, res, next) => {
-    var name = req.params.name;
-    //console.log(name);
-    User.getUserNames(name, (err, data) => {
-      data.dateOfJoin = new Date(data.dateOfJoin).toLocaleDateString(
+router.get("/all", config.checkToken, (req, res, next) => {
+  User.getUsers((err, data) => {
+    data.forEach(element => {
+      element.dateOfJoin = new Date(element.dateOfJoin).toLocaleDateString(
         "en"
       );
-      res.json(data);
     });
-  }
-);
+    res.json(data);
+  });
+});
 
-router.get(
-  "/getid/:id",
-  config.checkToken,
-  (req, res, next) => {
-    var id = req.params.id;
-    User.getUserById(id, (err, data) => {
-      data.dateOfJoin = new Date(data.dateOfJoin).toLocaleDateString(
-        "en"
-      );
-      res.json(data);
-    });
-  }
-);
+router.get("/get/:name", config.checkToken, (req, res, next) => {
+  var name = req.params.name;
+  //console.log(name);
+  User.getUserNames(name, (err, data) => {
+    data.dateOfJoin = new Date(data.dateOfJoin).toLocaleDateString("en");
+    res.json(data);
+  });
+});
+
+router.get("/getid/:id", config.checkToken, (req, res, next) => {
+  var id = req.params.id;
+  User.getUserById(id, (err, data) => {
+    data.dateOfJoin = new Date(data.dateOfJoin).toLocaleDateString("en");
+    res.json(data);
+  });
+});
 
 //Create
-router.post(
-  "/create",
-  config.checkToken,
-  (req, res, next) => {
-    let newUser = new User({
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      phone: req.body.phone,
-      email: req.body.email,
-      dateOfJoin: req.body.dateOfJoin,
-      username: req.body.username,
-      password: req.body.password,
-      type: req.body.type
-    });
-    User.addUser(newUser, (err, user) => {
-      if (err) {
-        res.json({ success: false, msg: "Failed to Add user." });
-      } else {
-        Room.getRoomById(req.body.roomId, (err, room) => {
-          if (err) {
-            res.json({ msg: "Failed while updating room", success: false });
-          } else {
-            room.user = user._id;
-            room.isVacant = false;
-            Room.updateRoom(room._id, room, (err, room) => {
-              var emailBody =
-                "Hi " +
-                user.firstName +
-                " " +
-                user.lastName +
-                ",</br> Your account has been created. Please find the username and password below.</br> " +
-                "Username: "+user.username +" and Password: "+req.body.password
-                ".</br>Thanks,</br>Admin.";
+router.post("/create", config.checkToken, (req, res, next) => {
+  let newUser = new User({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    phone: req.body.phone,
+    email: req.body.email,
+    dateOfJoin: req.body.dateOfJoin,
+    username: req.body.username,
+    password: req.body.password,
+    type: req.body.type
+  });
+  User.addUser(newUser, (err, user) => {
+    if (err) {
+      res.json({ success: false, msg: "Failed to Add user." });
+    } else {
+      Room.getRoomById(req.body.roomId, (err, room) => {
+        if (err) {
+          res.json({ msg: "Failed while updating room", success: false });
+        } else {
+          room.user = user._id;
+          room.isVacant = false;
+          Room.updateRoom(room._id, room, (err, room) => {
+            var emailBody =
+              "Hi " +
+              user.firstName +
+              " " +
+              user.lastName +
+              ",</br> Your account has been created. Please find the username and password below.</br> " +
+              "Username: " +
+              user.username +
+              " and Password: " +
+              req.body.password +
+              ".</br>Thanks,</br>Admin.";
 
-              var subject = "New Account Created.";
-              email.sendEmail(res, user.email, subject, emailBody,{ success: true, id: user._id, msg: "User Added." });
-            })
-          }
-        })
-      }
-    });
-  }
-);
+            var subject = "New Account Created.";
+            email.sendEmail(res, user.email, subject, emailBody, {
+              success: true,
+              id: user._id,
+              msg: "User Added."
+            });
+          });
+        }
+      });
+    }
+  });
+});
 
 //Update
-router.put(
-  "/update/:id",
-  config.checkToken,
-  function (req, res, next) {
-    //console.log( req.body);
-    var id = req.params.id;
-    var update = {
-      firstName: req.body.firstName,
-      lastName: req.body.lastName,
-      phone: req.body.phone,
-      email: req.body.email,
-      dateOfJoin: req.body.dateOfJoin,
-      username: req.body.username,
-      password: req.body.password,
-      type: req.body.type
-    };
-    User.updateUser(id, update, (err, user) => {
-      if (err) {
-        res.json({ msg: "Failed while updating user", success: false });
-      } else {
-        res.json({ msg: "user updated successfully", success: true, id: user._id });
-      }
-    });
-  }
-);
+router.put("/update/:id", config.checkToken, function(req, res, next) {
+  //console.log( req.body);
+  var id = req.params.id;
+  var update = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    phone: req.body.phone,
+    email: req.body.email,
+    dateOfJoin: req.body.dateOfJoin,
+    username: req.body.username,
+    password: req.body.password,
+    type: req.body.type
+  };
+  User.updateUser(id, update, (err, user) => {
+    if (err) {
+      res.json({ msg: "Failed while updating user", success: false });
+    } else {
+      res.json({
+        msg: "user updated successfully",
+        success: true,
+        id: user._id
+      });
+    }
+  });
+});
 
 //Delete
-router.delete(
-  "/delete/:id",
-  config.checkToken,
-  (req, res, next) => {
-    User.deleteUser(req.params.id, (err, result) => {
-      if (err) {
-        res.json({
-          msg: "Failed while deleting user",
-          status: "error",
-          success: false
-        });
-      } else {
-        Room.getRoomByUserId(req.params.id, (err, room) => {
-          if (err) {
-            res.json({ msg: "Failed while updating room", success: false });
-          } else {
-            room.isVacant = true;
-            room.user = null;
-            Room.updateRoom(room._id, room, (err, room) => {
-              res.json({ msg: "user deleted successfully", success: true });
-            })
-          }
-        })
-
-      }
-    });
-  }
-);
+router.delete("/delete/:id", config.checkToken, (req, res, next) => {
+  User.deleteUser(req.params.id, (err, result) => {
+    if (err) {
+      res.json({
+        msg: "Failed while deleting user",
+        status: "error",
+        success: false
+      });
+    } else {
+      Room.getRoomByUserId(req.params.id, (err, room) => {
+        if (err) {
+          res.json({ msg: "Failed while updating room", success: false });
+        } else {
+          room.isVacant = true;
+          room.user = null;
+          Room.updateRoom(room._id, room, (err, room) => {
+            res.json({ msg: "user deleted successfully", success: true });
+          });
+        }
+      });
+    }
+  });
+});
 
 module.exports = router;
